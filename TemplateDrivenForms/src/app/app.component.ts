c9d53b84b4c59bed3c7d4fefb9952756642e9bfe
Ahmed Sameh <ahmeds14@protonmail.ch>
2023-12-27 12:48:05 -0800

    endtemplatedriven

--- TemplateDrivenForms/src/app/app.component.ts
+++ TemplateDrivenForms/src/app/app.component.ts
@@ -12,10 +12,41 @@ export class AppComponent {
   fName = '';
   lName = '';
   UserEmail = '';
+  DateOfBirth = '';
+
   OnFormSubmited() {
-    console.log(this.fName);
-    console.log(this.lName);
-    console.log(this.UserEmail);
+    console.log(this.Form);
+    // this.Form.reset({
+    //   gender: 'male',
+    //   address: {
+    //     Country: '', // Set the default value for the country control
+    //   },
+    // });
   }
+  IsAgreed = false;
   @ViewChild('RegisterForm') Form: NgForm;
+  @ViewChild('FormGroub') FormGroub: NgForm;
+  CalcUserName() {
+    let userName = '';
+    if (this.fName.length >= 3) {
+      userName += this.fName.slice(0, 3);
+    } else {
+      userName += this.fName;
+    }
+    userName += '_';
+    if (this.lName.length >= 3) {
+      userName += this.lName.slice(0, 3);
+    } else {
+      userName += this.lName;
+    }
+    userName += '_';
+
+    let BirthYear = new Date(this.DateOfBirth);
+    userName += BirthYear.getFullYear();
+    userName = userName.toLocaleLowerCase();
+    this.Form.value.username = userName;
+    this.Form.form.patchValue({
+      username: userName,
+    });
+  }
 }
