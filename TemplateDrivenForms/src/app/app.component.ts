c81bc517e1f2fa9964a1121c8a173c57b34ac688
Ahmed Sameh <ahmeds14@protonmail.ch>
2023-12-27 02:51:05 -0800

    validation

--- TemplateDrivenForms/src/app/app.component.ts
+++ TemplateDrivenForms/src/app/app.component.ts
@@ -1,10 +1,21 @@
+import { ViewChild } from '@angular/core';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'TemplateDrivenForms';
+  fName = '';
+  lName = '';
+  UserEmail = '';
+  OnFormSubmited() {
+    console.log(this.fName);
+    console.log(this.lName);
+    console.log(this.UserEmail);
+  }
+  @ViewChild('RegisterForm') Form: NgForm;
 }
