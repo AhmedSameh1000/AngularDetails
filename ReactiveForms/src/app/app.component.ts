c50365eb2455cd6e008f4a140add50db88b8a696
Ahmed Sameh <ahmeds14@protonmail.ch>
2023-12-28 05:56:56 +0200

    StartReactiveForm

--- ReactiveForms/src/app/app.component.ts
+++ ReactiveForms/src/app/app.component.ts
@@ -1,10 +1,35 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.RegisterForm();
+  }
+  RegisterForm() {
+    this.ReactiveForm = new FormGroup({
+      FirstName: new FormControl(null, Validators.required),
+      LastName: new FormControl(null, Validators.required),
+      Email: new FormControl(null, [Validators.required, Validators.email]),
+      UserName: new FormControl(null),
+      DateOfBirth: new FormControl(null),
+      Street: new FormControl(null),
+      Gender: new FormControl(null),
+      Country: new FormControl(['America', 'India']),
+      City: new FormControl(null),
+      Region: new FormControl(null),
+      Code: new FormControl(null),
+    });
+  }
   title = 'ReactiveForms';
+  ReactiveForm: FormGroup;
+  OnFormSubmited() {
+    console.log(this.ReactiveForm);
+  }
 }
